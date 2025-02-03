4cf3009cb46c41b0f88ffa3719985a3d6404841f
Lenoch <81lennoch@gmail.com>
2025-02-03 13:58:02 +0100

    Fix discord app dependance for console icon
    Now using direct icon url from retroachievements.org instead of discord app icons

--- utils/presence.py
+++ utils/presence.py
@@ -1,17 +1,14 @@
-import re
 from utils import get_config
-from utils.retroachievements import get_game_icon
+from utils.retroachievements import get_game_icon, get_console_icon
 
 config = get_config()
 
 
-def get_release_year(release_date):
+def get_release_year(release_date: str) -> str:
     return release_date.split()[-1][:4]
 
-def sanitize_console_name(console_name):
-    sanitized_name = re.sub('[^0-9a-zA-Z]+', '', console_name)
-    return sanitized_name.lower()
 
+    
 def update_presence(RPC, data, game_data, start_time):
     year_of_release = get_release_year(game_data['Released'])
     details = f"{game_data['GameTitle']} ({year_of_release})"
@@ -22,7 +19,7 @@ def update_presence(RPC, data, game_data, start_time):
         start=start_time,
         large_image=get_game_icon(game_data),
         large_text=f"Released {game_data['Released']}, Developed by {game_data['Developer']}, Published by {game_data['Publisher']}",
-        small_image=sanitize_console_name(game_data['ConsoleName']),
+        small_image=get_console_icon(game_data['ConsoleID']),
         small_text=game_data['ConsoleName'],
         buttons=[{"label": "View RA Profile", "url": f"https://retroachievements.org/user/{data.get('User', config.get('ra_username'))}"}]
     )
--- utils/retroachievements/medias.py
+++ utils/retroachievements/medias.py
@@ -1,4 +1,11 @@
 import requests
+from utils.get_config import get_config
+from .get_data import get_http
+
+config = get_config()
+api_key = config.get('ra_api_key')
+consoles = get_http(f"https://retroachievements.org/API/API_GetConsoleIDs.php?y={api_key}")
+
 
 def get_game_icon(game_data: dict) -> str:
     game_icon = game_data.get('GameIcon') or game_data.get('ImageBoxArt')
@@ -9,4 +16,10 @@ def get_game_icon(game_data: dict) -> str:
         if response.status_code == 200:
             return image_url
     
+    return "ra_logo"
+
+def get_console_icon(console_id: int) -> str:
+    for i in consoles:
+        if i.get("ID", -1) == console_id:
+            return i.get("IconURL", "ra_logo")
     return "ra_logo"
